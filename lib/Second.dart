4ce4a7d877957166ebfc4c5f9e115266057568fd
anik11556 <abdullahal-2019317827@cs.du.ac.bd>
2023-01-13 12:52:22 +0600

    done

---
+++ lib/Second.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:testjson/main.dart';
+
+class Second extends StatefulWidget {
+  const Second({Key? key}) : super(key: key);
+
+  @override
+  State<Second> createState() => _SecondState();
+}
+
+class _SecondState extends State<Second> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Title"),),
+
+      floatingActionButton: FloatingActionButton(onPressed: ()
+    {
+     // Navigator.pop(context);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(title: 'Hello',)));
+
+      },),
+    );
+  }
+}
